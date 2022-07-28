d294ba65cc0f617e5353a04bb94b6eb91d41e495
Kirill Snopko <kirillsnopko@gmail.com>
2022-07-28 10:58:08 +0300

    Update: started doing charts

--- FinApp/Scripts/my/badges.js
+++ FinApp/Scripts/my/badges.js
@@ -1,5 +1,5 @@
 ﻿$(window).on('load', function () {
-    $.get("../Depository/Count", {},
+    $.get("/Depository/Count", {},
         function (counts) {
             $('#dep_count').html(counts['dep_count']);
             $('#credit_count').html(counts['credit_count']);
