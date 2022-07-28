2eaf27e22d41a1c62d60a4057db0d5f73ebec589
Kirill Snopko <kirillsnopko@gmail.com>
2022-07-28 08:55:35 +0300

    Update: added category field

--- FinApp/Entities/Finance/FinanceOperation.cs
+++ FinApp/Entities/Finance/FinanceOperation.cs
@@ -8,8 +8,14 @@ namespace FinApp.Entities.Finance
         public string idUser { get; set; }
         public int idDepository { get; set; }
         public double amountOfMoney { get; set; }
+        public Category category { get; set; }
         public string comment { get; set; }
         public DateTime created { get; set; }
         public bool isSpending { get; set; }
+    }
+
+    public enum Category
+    {
+        Home, Repair, Supermarkets, Pharmacy, Entertainment, Transport, Clothing, Electronics, Others
     }
 }
